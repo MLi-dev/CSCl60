634d1053f44431be0df531b374cc57654a76b422
Matthew Li <matthewli2003@gmail.com>
2022-02-01 09:46:48 -0800

    Fully defined constructors and member functions for automobile

--- Automobile/automobile.cpp
+++ Automobile/automobile.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "automobile.h"
+
+automobile::automobile() {
+    make_ = "";
+    model_ = "";
+    year_ = 0;
+    price_ = 0.00;
+    stall_ = 0;
+}
+automobile::automobile(std::string autoMake, std::string autoModel, int autoYear, double autoPrice, int autoStall) {
+    make_ = autoMake;
+    model_ = autoModel;
+    year_ = autoYear;
+    price_ = autoPrice;
+    stall_ = autoStall;
+
+}
+
+std::string automobile::getMake() {
+    return make_;
+}
+
+std::string automobile::getModel() {
+    return model_;
+}
+int automobile::getYear() {
+    return year_;
+}
+double automobile::getPrice() {
+    return price_;
+}
+int automobile::getStall() {
+    return stall_;
+}
+void automobile::setStall(int autoStall) {
+    stall_ = autoStall;
+}
+bool automobile::park(bool *lot, int size) {
+
+}
--- Automobile/automobile.h
+++ Automobile/automobile.h
@@ -4,9 +4,26 @@
 
 #ifndef AUTOMOBILE_AUTOMOBILE_H
 #define AUTOMOBILE_AUTOMOBILE_H
+#include <string>
 
 
 class automobile {
+public:
+    automobile();
+    automobile(std::string autoMake, std::string autoModel, int autoYear, double autoPrice, int autoStall);
+    std::string getMake();
+    std::string getModel();
+    int getYear();
+    double getPrice();
+    int getStall();
+    void setStall(int autoStall);
+    bool park(bool lot[], int size);
+private:
+    std::string make_;
+    std::string model_;
+    int year_;
+    double price_;
+    int stall_;
 
 };
 
