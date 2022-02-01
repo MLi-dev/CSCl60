2b18bb8a8fb93d99b64f3acee243e24b7a5309d0
Matthew Li <matthewli2003@gmail.com>
2022-01-31 20:40:41 -0800

    Added getters for the member functions, default constructor, and constructor to set member functions.

---
+++ Automobile/automobile.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matthew Li on 1/31/22.
+//
+
+#include "automobile.h"
---
+++ Automobile/automobile.h
@@ -0,0 +1,14 @@
+//
+// Created by Matthew Li on 1/31/22.
+//
+
+#ifndef AUTOMOBILE_AUTOMOBILE_H
+#define AUTOMOBILE_AUTOMOBILE_H
+
+
+class automobile {
+
+};
+
+
+#endif //AUTOMOBILE_AUTOMOBILE_H
